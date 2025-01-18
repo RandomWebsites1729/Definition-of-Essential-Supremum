672feecb31cf797acf0b3c9995d4ee07da0b06b1
atrajit-sarkar <atrajit.sarkar@gmail.com>
2025-01-18 17:35:56 +0530

    fix: update HTML title and content title handling in markdown rendering

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 // Paste the link of your mark down file here to render it into your site
-const myMarkDownFile="index.md";
+const myMarkDownFile = "index.md";
+
+/* Function to fetch from the markdown file title of the website and content title...
+ in the markdown:
+    title: Title of the website
+    content-title: Title of the content
+    Rest of the content
+    
+    If the title is not found, it will set the title to "Default Title"
+    If the content title is not found, it will set the content title to "Default Content"
+    Rest will be taken as the content of the website
+ */
+
+async function getmdTitle(con) {
+    let titleMatch = con.match(/title: (.*)/);
+    let contentMatch = con.match(/content-title: (.*)/);
+    let title = titleMatch ? titleMatch[1] : "Default Title";
+    let content = contentMatch ? contentMatch[1] : "Default Content";
+    document.title = title;
+    document.querySelector(".title p").innerHTML = content;
+    return con.replace(/title: .*[\r\n]+content-title: .*[\r\n]+/, '');
+}
 
 // Fetch the Markdown file and render it
 async function getmd() {
     let res = await fetch(myMarkDownFile);
     let con = await res.text();
+    con = await getmdTitle(con);
     document.getElementsByClassName("md")[0].innerHTML = marked.parse(con);
 
     // Add copy buttons to code blocks after rendering
     addCopyButtons();
-    
+
     // Apply Prism.js syntax highlighting after rendering
     Prism.highlightAll();
 
     // Render Equations
     MathJax.typeset();
 }
+
 
 // Function to add copy buttons to all <pre><code> blocks
 function addCopyButtons() {
